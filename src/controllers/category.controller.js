bd48d30eb560757eb9199770716809cc42eadb2f
Tai272620 <tai272620967@gmail.com>
2023-07-27 14:51:59 +0700

    update new

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,7 +1,20 @@
 import categoryModel from '../models/category.model';
 
 module.exports = {
-    create: function (req, res) {
-        console.log("da vao controller create category")
+    create: async function (req, res) {
+        // console.log("da vao controller create category", req.body)
+        try {
+            let result = await categoryModel.create(req.body)
+            if (result.status) {
+                return res.status(200).json({
+                    message: result.message,
+                    data: result.data
+                })
+            }
+        } catch (err) {
+            return res.status(500).json({
+                message: "Lỗi không xác định!"
+            })
+        }
     }
 }
